2424549cb4a7625e394e9757d82123593e56e322
Alex Ong <alex.ong@unsw.edu.au>
2019-05-08 13:49:43 +1000

    Working autoshiftcache and powermate.py

--- autoshiftcache.py
+++ autoshiftcache.py
@@ -1,17 +1,105 @@
-
-
-class AutoShiftCache(object):    
+from sendinput import *
+def clamp(smol, big, value):
+    value = max(smol,min(value,big))
+    return value
+#override this class to make your input work.
+class InputHandler(object):    
     def __init__(self):
         self.counter = 0
-        self.delay = 0.016
-        self.sensitivity = 1 #higher numbers = less sensitive
+        self.isPushed = False        
         
     #Increment. can be by a negative amount
     def increment(self, amount):
         pass
         
+    def decrement(self, amount):
+        pass
+    
+    def updatePush(self, pushValue):
+        self.isPushed = pushValue
+        
     def tick(self, timestamp):
         pass
-        
 
+#Auto shifting handler. changing direction instantly stops.
+SIXTY_HZ = 1.0/60
+SENSITIVITY = 8
+SHIFT_MAX = 6
+class AutoShiftCache(InputHandler):
+    def __init__(self):
+        super(AutoShiftCache, self).__init__()
+        #pushlength is one cycle.
+        #scroll-length is 0.016
+        self.lastMessageTime = 0
+        self.currentTime = 0
+        self.leftIsDown = False
+        self.rightIsDown = False
+        self.lastWheelTime = 0
+        self.rawCounter = 0
+    def increment(self, amount):
+        if self.counter < 0:
+            self.counter = 0
+        else:
+            self.rawCounter += amount
+            while self.rawCounter > SENSITIVITY:
+                self.rawCounter -= SENSITIVITY
+                self.counter += 1
+        self.counter = clamp(0,SHIFT_MAX,self.counter)
         
+        self.lastMessageTime = self.currentTime
+    
+    def decrement(self, amount):
+        if self.counter > 0:
+            self.counter = 0
+        else:
+            self.rawCounter -= amount
+            while self.rawCounter < -SENSITIVITY:
+                self.rawCounter += SENSITIVITY
+                self.counter -= 1
+                
+        self.counter = clamp(-SHIFT_MAX,0,self.counter)
+        self.lastMessageTime = self.currentTime
+        
+    def tick(self, timestamp):
+        delta = timestamp - self.currentTime
+        eventDelta = timestamp - self.lastWheelTime
+        if self.counter > 0:
+            if self.leftIsDown:
+                ReleaseKey(VK_LEFT)
+            if eventDelta >= SIXTY_HZ:            
+                if self.rightIsDown:
+                    print("releaseRight")
+                    ReleaseKey(VK_RIGHT)    
+                    self.rightIsDown = False
+                    self.counter -= 1
+                else: #right isnt down.
+                    PressKey(VK_RIGHT)                    
+                    print("PressRight")
+                    self.rightIsDown = True
+        
+                self.lastWheelTime = timestamp
+        elif self.counter < 0:
+            if self.rightIsDown:
+                ReleaseKey(VK_RIGHT)
+            if eventDelta >= SIXTY_HZ:            
+                if self.leftIsDown:
+                    print("releaseLeft")
+                    ReleaseKey(VK_LEFT)    
+                    self.leftIsDown = False
+                    self.counter += 1
+                else: #right isnt down.
+                    PressKey(VK_LEFT)                   
+                    print("PressLeft")
+                    self.leftIsDown = True
+        
+                self.lastWheelTime = timestamp
+                
+        else: # self.counter == 0:
+            if self.rightIsDown:
+                ReleaseKey(VK_RIGHT)
+                self.lastWheelTime = timestamp
+            if self.leftIsDown:
+                ReleaseKey(VK_LEFT)
+                self.lastWheelTime = timestamp
+                
+        self.currentTime = timestamp
--- powermate.py
+++ powermate.py
@@ -3,7 +3,8 @@ from sendinput import PressKey, ReleaseKey, VK_LEFT, VK_RIGHT
 from autoshiftcache import AutoShiftCache
 import threading
 import time
-vid = "077d"
+
+vid = "077d" #vid and pid of powermate usb.
 pid = "0410"
 
 
@@ -17,37 +18,28 @@ class PowerMateReader(threading.Thread):
         result = api.list_usb_devices(deviceinterface=True, present=True)
         if result:
           if api.init_winusb_device(vid, pid):
-            dascache = 0    
             while True:
-                data = api.read(0x81, 6)        
-                offset = ord(data[1])
-                print(offset)
-                if offset > 127:
-                    offset = -(256 - offset)
+                #try:
+                    data = api.read(0x81, 6) #read 6 bytes. 
+                    buttonDown = ord(data[0])
+                    offset = ord(data[1])
+                    self.shiftCache.updatePush(buttonDown == 1)
                     
-                if offset != 0:
-                    if dascache == 0:
-                        dascache = offset
-                    else:
-                        if dascache > 0 and offset < 0:
-                            dascache = 0
-                        elif dascache < 0 and offset > 0:
-                            dascache = 0
-                        else:
-                            dascache += offset
-                while dascache >= 3:
-                    dascache -= 3
-                    PressKey(VK_RIGHT)
-                    time.sleep(0.016)
-                    ReleaseKey(VK_RIGHT)
-                    print("RIGHT", dascache)
-                while dascache <= -3:
-                    dascache += 3
-                    PressKey(VK_LEFT)
-                    time.sleep(0.016)
-                    ReleaseKey(VK_LEFT)
-                    print("LEFT", dascache)
+                    if offset > 127:
+                        offset = -(256 - offset)
+                    
+                    if offset > 0:
+                        self.shiftCache.increment(offset)
+                    elif offset < 0:
+                        self.shiftCache.decrement(-offset)
+                #except:
+                #    break
                     
 if __name__ == "__main__":
     pmr = PowerMateReader()
     pmr.start()
+    while True:
+        if not pmr.isAlive():
+            break
+        pmr.shiftCache.tick(time.time())
+    print ("Device not found, or disconnected. Exiting")
